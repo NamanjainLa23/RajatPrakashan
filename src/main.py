14efae25b43c41d5c63d4890fcf5787f963251a9
namanjn23 <naman.jain@xperi.com>
2025-01-19 22:57:34 +0530

    added calculation for bill

--- src/main.py
+++ src/main.py
@@ -1,9 +1,69 @@
 from tkinter import *
+from tkinter import messagebox
+import os
+
+global l
+l = []
+
+# Function to get the next bill number
+def get_next_bill_number():
+    filename = "bill_number.txt"
+    if os.path.exists(filename):
+        with open(filename, "r") as file:
+            last_bill = int(file.read())
+    else:
+        last_bill = 0  # Start from 1 if file doesn't exist
+
+    new_bill = last_bill + 1
+    with open(filename, "w") as file:
+        file.write(str(new_bill))
+    
+    return str(new_bill)
+
 root=Tk()
 root.title("Rajat Prakashan")
 
 root.geometry("1280x720")
 bg_color="#4D0039"
+
+
+c_name = StringVar()
+c_phone = StringVar()
+item = StringVar()
+rate = IntVar()
+qty = IntVar()
+bill_no = StringVar()
+
+bill_no.set(get_next_bill_number())
+
+def welcome():
+    text_area.delete(1.0,END)
+    text_area.insert(END,"\t\tRAJAT PRAKASHAN\n")
+    text_area.insert(END,f"\n====================================================")
+    text_area.insert(END,f"\nBill Number: \t\t{bill_no.get()}")
+    text_area.insert(END,f"\nCustomer Name: \t\t{c_name.get()}")
+    text_area.insert(END,f"\nPhone Number: \t\t{c_phone.get()}")
+    text_area.insert(END,f"\n====================================================\n")
+    text_area.insert(END,f"\n\nProduct Name\t\t\tQty\tPrice\tTotal\n")
+    text_area.configure(font="arial 15 bold")
+
+def add_item():
+    total = qty.get()*rate.get()
+    if item.get() == '' or rate.get() == 0 or qty.get() == 0:
+        messagebox.showerror("Error", "Please enter product name")
+    else:
+        text_area.insert(END,f"{item.get()}\t\t\t{qty.get()}\t{rate.get()}\t{total}\n")
+    l.append(total)
+
+def generate_bill():
+    tex = text_area.get(10.0,(10.0+float(len(l))))
+    welcome()
+    text_area.insert(END,tex)
+    text_area.insert(END,f"\n====================================================")
+    text_area.insert(END,f"\nTotal Bill Amount: \t\t\t{sum(l)}")
+    text_area.insert(END,f"\n====================================================")
+
+
 
 title = Label(root,text="Billing Software",font=("times new roman",40,"bold"),bg=bg_color,fg='white', relief=GROOVE)
 title.pack(fill=X)
@@ -16,13 +76,13 @@ F1.place(x=0,y=80,relwidth=1)
 cname_lbl = Label(F1, text="Customer Name",font=("times new roman",18,"bold"),bg=bg_color,fg='white')
 cname_lbl.grid(row=0,column=0,padx=20,pady=5)
 
-cname_txt = Entry(F1,width=15,font="arial 15",bd=3,relief=SUNKEN)
+cname_txt = Entry(F1,width=15,font="arial 15",bd=3,relief=SUNKEN, textvariable=c_name)
 cname_txt.grid(row=0,column=1,padx=10,pady=5)
 
 cphone_lbl = Label(F1, text="Phone Number",font=("times new roman",18,"bold"),bg=bg_color,fg='white')
 cphone_lbl.grid(row=0,column=2,padx=20,pady=5)
 
-cphone_txt = Entry(F1,width=15,font="arial 15",bd=3,relief=SUNKEN)
+cphone_txt = Entry(F1,width=15,font="arial 15",bd=3,relief=SUNKEN, textvariable=c_phone)
 cphone_txt.grid(row=0,column=3,padx=10,pady=5)
 
 
@@ -34,28 +94,28 @@ F2.place(x=20,y=180,width=630, height=500)
 itm_lbl = Label(F2, text="Product name",font=("times new roman",18,"bold"),bg=bg_color,fg='light green')
 itm_lbl.grid(row=0,column=0,padx=30,pady=20)
 
-itm_txt = Entry(F2,width=20,font="arial 15",bd=3,relief=SUNKEN)
+itm_txt = Entry(F2,width=20,font="arial 15",bd=3,relief=SUNKEN, textvariable=item)
 itm_txt.grid(row=0,column=1,padx=10,pady=20)
 
 rate_lbl = Label(F2, text="Product price",font=("times new roman",18,"bold"),bg=bg_color,fg='light green')
 rate_lbl.grid(row=1,column=0,padx=30,pady=20)
 
-rate_txt = Entry(F2,width=20,font="arial 15",bd=3,relief=SUNKEN)
+rate_txt = Entry(F2,width=20,font="arial 15",bd=3,relief=SUNKEN, textvariable=rate)
 rate_txt.grid(row=1,column=1,padx=10,pady=20)
 
 qty_lbl = Label(F2, text="Product quantity",font=("times new roman",18,"bold"),bg=bg_color,fg='light green')
 qty_lbl.grid(row=2,column=0,padx=30,pady=20)
 
-qty_txt = Entry(F2,width=20,font="arial 15",bd=3,relief=SUNKEN)
+qty_txt = Entry(F2,width=20,font="arial 15",bd=3,relief=SUNKEN, textvariable=qty)
 qty_txt.grid(row=2,column=1,padx=10,pady=20)
 
 
 #==========================Button=========================
 
-btn1 = Button(F2,text="Add Item",font="arial 15 bold",bg="lime",fg="black",width=15, padx=10)
+btn1 = Button(F2,text="Add Item",font="arial 15 bold",bg="lime",fg="black",width=15, padx=10, command=add_item)
 btn1.grid(row=3,column=0,padx=10,pady=20)
 
-btn2 = Button(F2,text="Generate bill",font="arial 15 bold",bg="lime",fg="black",width=15, padx=10)
+btn2 = Button(F2,text="Generate bill",font="arial 15 bold",bg="lime",fg="black",width=15, padx=10, command=generate_bill)
 btn2.grid(row=3,column=1,padx=10,pady=20)
 
 btn3 = Button(F2,text="Clear",font="arial 15 bold",bg="lime",fg="black",width=15, padx=10)
@@ -80,6 +140,8 @@ scroll_y.pack(side=RIGHT, fill=Y)
 
 scroll_y.config(command=text_area.yview)
 text_area.pack(fill=BOTH, expand=1)
+
+welcome()
 
 
 root.mainloop()
